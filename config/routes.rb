79eb07e01e901d233e5e4b0107401f656dc3faca
EC2 Default User <ec2-user@ip-172-31-7-208.ec2.internal>
2021-03-10 12:39:30 +0000

    サーチのルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す 
+  get '/search' => 'search#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
