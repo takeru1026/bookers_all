a7c0a95386030c8ee32d26510a6b78d65907e670
EC2 Default User <ec2-user@ip-172-31-7-208.ec2.internal>
2021-07-06 09:03:45 +0000

    非同期通信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,20 +3,13 @@ class CommentsController < ApplicationController
     @book=Book.find(params[:book_id])
     @comment= current_user.comments.new(comment_params)
     @comment.book_id = @book.id
-    if @comment.save
-      redirect_back(fallback_location: books_path)
-    else
-      @book=Book.find(params[:book_id])
-      @comment_new = @comment
-      render 'books/show'
-    end
+    @comment.save!
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @comment = @book.comments.find(params[:id])
-    @comment.destroy
-    redirect_back(fallback_location: books_path)
+    @comment.destroy!
   end
 
   private
